fc95a568ce9ebe4cb68d3321d59b62c96780fe9d
pacimarco <m.paci1987@gmail.com>
2022-07-18 17:47:33 +0200

    stampa in console delle proprietà

---
+++ js/main.js
@@ -0,0 +1,41 @@
+const membroTeam = [
+    {
+        nome: 'Wayne Barnett',
+        ruolo: 'Founder & CEO',
+        immagine: 'wayne-barnett-founder-ceo.jpg',
+    },
+    {
+        nome: 'Angela Caroll',
+        ruolo: 'Chief Editor',
+        immagine: 'angela-caroll-chief-editor.jpg',
+    },
+    {
+        nome: 'Walter Gordon',
+        ruolo: 'Office Manager',
+        immagine: 'walter-gordon-office-manager.jpg',
+    },
+    {
+        nome: 'Angela Lopez',
+        ruolo: 'Social Media Manager',
+        immagine: 'angela-lopez-social-media-manager.jpg',
+    },
+    {
+        nome: 'Scott Estrada',
+        ruolo: 'Developer',
+        immagine: 'scott-estrada-developer.jpg',
+    },
+    {
+        nome: 'Barbara Ramos',
+        ruolo: 'Graphic Designer',
+        immagine: 'barbara-ramos-graphic-designer.jpg',
+    },
+    
+]
+
+
+
+for (let chiave in membroTeam){
+    console.log(membroTeam[chiave].nome);
+    console.log(membroTeam[chiave].ruolo);
+    console.log(membroTeam[chiave].immagine);
+}
